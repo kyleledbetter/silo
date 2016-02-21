c67cbce94240be2091e467b116e63e969b196b6f
kyleledbetter <kyle@kyleledbetter.com>
2016-02-21 15:35:59 -0600

    update(data)

--- client/app/files/files.controller.js
+++ client/app/files/files.controller.js
@@ -8,41 +8,41 @@ angular.module('siloApp')
     };
 
     // Mock activity
-    $scope.activity = [
+  $scope.activity = [
       {
-        what: 'Brunch this weekend?',
+        what: 'filename_etc.jpg',
         who: 'Ali Conners',
         avatar: 'svg-1',
-        when: '3:08PM',
-        notes: ' I\'ll be in your neighborhood doing errands'
+        when: 'Feb 20, 2016 at 3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
       },
       {
-        what: 'Summer BBQ',
-        who: 'to Alex, Scott, Jennifer',
+        what: 'filename_etc.pdf',
+        who: 'Don Jonson',
         avatar: 'svg-2',
-        when: '3:08PM',
-        notes: 'Wish I could come out but I\'m out of town this weekend'
+        when: 'Feb 20, 2016 at 3:08PM',
+        notes: "Wish I could come out but I'm out of town this weekend"
       },
       {
-        what: 'Oui Oui',
+        what: 'filename_etc.xls',
         who: 'Sandra Adams',
         avatar: 'svg-3',
-        when: '3:08PM',
-        notes: 'Do you have Paris recommendations? Have you ever been?'
+        when: 'Feb 20, 2016 at 3:08PM',
+        notes: "Do you have Paris recommendations? Have you ever been?"
       },
       {
-        what: 'Birthday Gift',
+        what: 'filename_etc.doc',
         who: 'Trevor Hansen',
         avatar: 'svg-4',
-        when: '3:08PM',
-        notes: 'Have any ideas of what we should get Heidi for her birthday?'
+        when: 'Feb 20, 2016 at 3:08PM',
+        notes: "Have any ideas of what we should get Heidi for her birthday?"
       },
       {
-        what: 'Recipe to try',
+        what: 'filename_etc.png',
         who: 'Brian Holt',
         avatar: 'svg-5',
-        when: '3:08PM',
-        notes: 'We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos'
+        when: 'Feb 20, 2016 at 3:08PM',
+        notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos"
       },
     ];
   });
--- client/app/questions/questions.controller.js
+++ client/app/questions/questions.controller.js
@@ -7,42 +7,41 @@ angular.module('siloApp')
       $mdSidenav('left').toggle();
     };
 
-    // Mock activity
     $scope.activity = [
       {
         what: 'Brunch this weekend?',
         who: 'Ali Conners',
         avatar: 'svg-1',
         when: '3:08PM',
-        notes: ' I\'ll be in your neighborhood doing errands'
+        notes: "I'll be in your neighborhood doing errands. Selfies tumblr dreamcatcher, wayfarers mlkshk master cleanse mixtape man bun tofu you probably haven't heard of them pitchfork banjo."
       },
       {
-        what: 'Summer BBQ',
+        what: 'Summer BBQ?',
         who: 'to Alex, Scott, Jennifer',
         avatar: 'svg-2',
         when: '3:08PM',
-        notes: 'Wish I could come out but I\'m out of town this weekend'
+        notes: "Wish I could come out but I'm out of town this weekend. Selfies tumblr dreamcatcher, wayfarers mlkshk master cleanse mixtape man bun tofu you probably haven't heard of them pitchfork banjo."
       },
       {
-        what: 'Oui Oui',
+        what: 'Oui Oui?',
         who: 'Sandra Adams',
         avatar: 'svg-3',
         when: '3:08PM',
-        notes: 'Do you have Paris recommendations? Have you ever been?'
+        notes: "Do you have Paris recommendations? Have you ever been? Selfies tumblr dreamcatcher, wayfarers mlkshk master cleanse mixtape man bun tofu you probably haven't heard of them pitchfork banjo."
       },
       {
-        what: 'Birthday Gift',
+        what: 'Birthday Gift?',
         who: 'Trevor Hansen',
         avatar: 'svg-4',
         when: '3:08PM',
-        notes: 'Have any ideas of what we should get Heidi for her birthday?'
+        notes: "Have any ideas of what we should get Heidi for her birthday?"
       },
       {
-        what: 'Recipe to try',
+        what: 'Recipe to try?',
         who: 'Brian Holt',
         avatar: 'svg-5',
         when: '3:08PM',
-        notes: 'We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos'
+        notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos. Selfies tumblr dreamcatcher, wayfarers mlkshk master cleanse mixtape man bun tofu you probably haven't heard of them pitchfork banjo."
       },
     ];
   });
